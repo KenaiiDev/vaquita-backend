c3212ecb4c38584047ad76f6a35c33610ebca8a9
Lucas Villanueva <lucasvillanueva.dev@gmail.com>
2025-04-29 23:00:38 +0000

    chore: update CI workflow and improve type definitions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req: any, res: any) => {
+app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello, World!");
 });
 
